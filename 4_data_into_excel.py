fbe53c8558fa38c7bff386dd09d3c1a57a682d3f
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-09-19 23:41:05 +0500

    How to Convert Data Into Excel File

---
+++ 4_data_into_excel.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data={
+    "Name":["Shayan","Noman","Sanan","Hamdan","ayyan"],
+    "City":["Swabi","Dagi","Lahore","Swabi","Rashaka"],
+    "age":[11,22,33,44,55]
+}
+df=pd.DataFrame(data)
+print(df)
+
+df.to_excel("Home_Data.xlsx")
