13e85e441e7313f0f687f9dff5c17bb2b309ec96
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-09-24 22:11:35 +0500

    How to Drop Column from Dataset

---
+++ 22_Droping_column.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+data=pd.read_excel("EmployeeData.xlsx")
+print(data)
+
+
+# adding column diectly
+
+data["Bonus"]=data["Salary"]*0.1
+data["Total_Salary"]=data["Salary"]+data["Bonus"]
+print(data)
+
+# Another Method mean according to location.....
+
+data.insert(0, "ID", [11, 22, 44, 55, 66, 77, 88, 99])
+print(data)
+
+data.drop(columns=["Bonus","Total_Salary"],inplace=True)
+print(data)
