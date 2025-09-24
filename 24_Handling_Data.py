db6e609a467ecd8859a5d3d51bd34bca487a1ce3
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-09-24 22:12:38 +0500

    How to Handle data in Dataset

---
+++ 24_Handling_Data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+# Dictionary with 10 employees and some missing values
+employees = {
+    1: {"Name": "Ali",   "Age": 25, "Salary": 40000},
+    2: {"Name": "Sara",  "Age": None, "Salary": 50000},
+    3: {"Name": None,    "Age": 30, "Salary": 45000},
+    4: {"Name": "Hina",  "Age": 28, "Salary": None},
+    5: {"Name": "Usman", "Age": 35, "Salary": 60000},
+    6: {"Name": "Zara",  "Age": None, "Salary": None},
+    7: {"Name": "Amir",  "Age": 40, "Salary": 70000},
+    8: {"Name": None,    "Age": 22, "Salary": 30000},
+    9: {"Name": "Nida",  "Age": 27, "Salary": None},
+    10: {"Name": "Fahad", "Age": None, "Salary": 55000}
+}
+
+data=pd.DataFrame(employees)
+data.to_excel("Shayan.xlsx")
+print(data)
+
+
+# how to remove none values
+data.dropna(inplace=True)
+print(data)
+
+# how to fill values in nullllll
+
+data.fillnai(0)
